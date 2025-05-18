488f61c5ca95a3e68e3067b6a5baf76d76837a77
kristinadd <kristina@productboard.com>
2025-05-18 20:13:34 +0200

    init

---
+++ src/main/java/com/uShop/UShopApplication.java
@@ -0,0 +1,13 @@
+package com.uShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uShop/UShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
